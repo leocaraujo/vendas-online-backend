a8053163c36ba90af1344fd3c50ae571f38f3905
leocaraujo <leo.carvalho.araujo@gmail.com>
2023-05-05 21:21:15 -0300

    feat: create user (POST)

---
+++ src/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+
+@Controller('user')
+export class UserController {
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto) {
+    return {
+      ...createUser,
+      password: '**********',
+    };
+  }
+
+  @Get()
+  async getAllUsers() {
+    return JSON.stringify({ test: 'abc' });
+  }
+}
