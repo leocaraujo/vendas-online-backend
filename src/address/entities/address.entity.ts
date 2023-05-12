e826f0cedefceba1958540849caacbe567cf5ae1
leocaraujo <leo.carvalho.araujo@gmail.com>
2023-05-12 19:52:02 -0300

    feat: insert controller address

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ name: 'adress' })
+@Entity({ name: 'address' })
 export class AddressEntity {
   @PrimaryGeneratedColumn('rowid')
   id: number;
