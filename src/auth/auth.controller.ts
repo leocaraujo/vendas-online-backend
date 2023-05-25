281acc21c449f6451e55ceaf4f28d3254b0cc417
leocaraujo <leo.carvalho.araujo@gmail.com>
2023-05-24 23:11:40 -0300

    feat: add token JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,7 @@ import {
 import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
 import { loginDto } from './dtos/login.dto';
 import { AuthService } from './auth.service';
+import { ReturnLogin } from './dtos/returnLogin.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -15,7 +16,7 @@ export class AuthController {
 
   @UsePipes(ValidationPipe)
   @Post()
-  async login(@Body() loginDto: loginDto): Promise<ReturnUserDto> {
-    return new ReturnUserDto(await this.authService.login(loginDto));
+  async login(@Body() loginDto: loginDto): Promise<ReturnLogin> {
+    return await this.authService.login(loginDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,18 @@ import { UserService } from 'src/user/user.service';
 import { loginDto } from './dtos/login.dto';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnLogin } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(loginDto: loginDto): Promise<UserEntity> {
+  async login(loginDto: loginDto): Promise<ReturnLogin> {
     const user: UserEntity = await this.userService
       .findUserByEmail(loginDto.email)
       .catch(() => undefined);
@@ -19,6 +26,9 @@ export class AuthService {
         `User with email ${loginDto.email} not found`,
       );
     }
-    return user;
+    return {
+      accessToken: this.jwtService.sign({ ...new LoginPayload(user) }),
+      user: new ReturnUserDto(user),
+    };
   }
 }
