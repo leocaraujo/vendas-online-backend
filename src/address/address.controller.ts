2651f74ef1a743780e466e43f4ac11d766ffd3eb
leocaraujo <leo.carvalho.araujo@gmail.com>
2023-05-27 15:18:25 -0300

    feat: add authorization jwt

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -9,11 +9,14 @@ import {
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enums/user-type.enum';
 
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
+  @Roles(UserType.User)
   @Post('/:userId')
   @UsePipes(ValidationPipe)
   async createAddress(
